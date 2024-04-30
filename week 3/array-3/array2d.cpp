43ae50c2973d62f63e52fe7e97cbfe6225f1cd62
iamrahulmeena <rahulmeenaoffical@gmail.com>
2024-05-01 02:17:56 +0530

    2d array

---
+++ week 3/array-3/array2d.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //creation
+    // int arr[5][3];
+
+
+    //inlitialse
+    int arr[3][3]={
+                    {5,2,3},
+                    {9,50,6},
+                    {5,6,7}
+                  }; 
+
+
+}
