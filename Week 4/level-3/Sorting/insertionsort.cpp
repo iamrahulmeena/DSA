9e27da2852c20ac212df2a18b029352fb24b3d31
iamrahulmeena <rahulmeenaoffical@gmail.com>
2024-06-01 00:55:45 +0530

    done

--- Week 4/level-3/Sorting/insertionsort.cpp
+++ Week 4/level-3/Sorting/insertionsort.cpp
@@ -5,16 +5,15 @@ int insertionsort(int arr[],int n){
 
     // for (int i = 1; i <n ; i++)
     // {
-
     //     int key=arr[i];
     //     int j = i-1;
     //     for (; j  >= 0 ; j--)
     //     {
     //         if(arr[j]>key){
-    //           arr[j+1]=arr[j];
+    //         arr[j+1]=arr[j];
     //         }
 
-    //         else{
+    //      else{
     //             break;
     //         }
     //     }
@@ -32,8 +31,6 @@ int insertionsort(int arr[],int n){
         }
         arr[j+1]=key;
     }
-    
-    
 }
 
 
